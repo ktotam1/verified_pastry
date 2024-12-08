56eaee985a70e82410a8c6401f8856954b7e2f24
alex <anithacan01@gmail.com>
2024-12-08 15:21:05 +0100

    redo everything a different way ¯\_(ツ)_/¯

--- pastry/Pastry.scala
+++ pastry/Pastry.scala
@@ -1,26 +1,28 @@
-import scala.collection.immutable.HashMap
 import stainless.collection.*
+import java.util.HashMap
+import stainless.lang.*
 
-trait Purpose
-case class Get(key: Int) extends Purpose
-case class Reply(value: String) extends Purpose
-case class NotFound() extends Purpose
-case class Set(key: Int, value: String) extends Purpose
-case class Gossip() extends Purpose // idk what this does yet, maybe exchanges routing tables or something 
-case class Empty() extends Purpose 
-case class Error(reason: String) extends Purpose
+def abs(x: Int): Int = {
+    if x < 0 then -x else x
+}
+def min(x: Int, y: Int): Int = {
+    if x > y then y else x
+}
+def max(x: Int, y: Int): Int = {
+    if x > y then x else y
+}
+def dist(x:Int, y:Int): Int = {
+    min(abs(x-y), abs(max(x,y)-Int.MaxValue - min(x,y)))
+}
+//ring less than 
+def rlt(x: Int,y: Int): Boolean = {
+    if abs(x-y) < abs(max(x,y)-Int.MaxValue - min(x,y)) then 
+        x < y
+    else 
+        x > y
+}   
 
-case class Message(to: Int, from: Int, purpose: Purpose)
-
-case class Node(id: Int, replicationFactor: Int) {
-    val values = HashMap[Int,String]()
-    val routingTable = HashMap[Int, Node]() 
-    val leafset = Array.ofDim[Node](replicationFactor)
-
-    var network: Network = Network(0) 
-
-    // shared prefix
-    def shl(a: Int, b: Int): Int = {
+def shl(a: Int, b: Int): Int = {
         var i = 0
         var done = false 
         while(i <= 32 && !done) {
@@ -30,53 +32,146 @@ case class Node(id: Int, replicationFactor: Int) {
                 i += 1
         }
         32-i
-    }
+}
 
-    def getNextHop(key: Int): Int = {
-        0
-    }
-
-    def get(key: Int): Option[String] = {
-        if values.contains(key) then
-            values.get(key)
-        else 
-            val nextHop = getNextHop(key)
-            network.send(Message(nextHop, id, Get(key))) match
-                case Reply(value) => Option(value)
-                case _ => None
-    }
-
-    def set(key: Int, value: String): Unit = {
-        val nextHop = getNextHop(key)
-        network.send(Message(nextHop, id, Set(key, value)))
-    }
-
-    def send(msg: Message): Purpose = { 
-        msg.purpose match {
-            case Get(key) => {
-                get(key) match 
-                    case None => Empty()
-                    case Some(value) => Reply(value)
-            }
-            case Set(key, value) => {
-                set(key, value)
-                Empty()
-            }
-            case _ => Empty()
+case class RoutingTable(id: Int) {
+    private var ids: MutableMap[Int, List[Int]] = MutableMap.withDefaultValue(() => List[Int]())
+    //returns the KEY with the largest matching prefix. -1 otherwise
+    def biggestMatchingPrefix(key: Int): Int = {
+        val l = shl(id, key)
+        def foreach(ids: List[Int], ans: Int , key: Int): Int = {
+            ids match   
+                case Nil() => key
+                case x :: xs =>
+                    if (shl(x,key) > ans) then
+                        foreach(xs, shl(x,key),x)
+                    else 
+                        foreach(xs, ans, key)
         }
+        foreach(ids(l), 0, -1)
+    }
+    def remove(id: Int): Unit = {
+        ids.update(shl(id, this.id), ids(shl(id,this.id)).withFilter(_ != id))
+    }
+    def add(id: Int): Unit = {
+        ids.update(shl(id, this.id), id :: ids(shl(id,this.id)))
+    }
+    def update(other: RoutingTable): Unit = {
+        val keys = List.fromScala((ids.theMap.keySet ++ other.ids.theMap.keySet).toList)
+        def updater(keys: List[Int]): Unit = {
+            keys match 
+                case x :: xs => 
+                    ids.update(x, ids(x) ++ other.ids(x))
+                    updater(xs)
+        }
+        updater(keys)
+    }
+    def idList(): List[Int] = {
+        val keys = List.fromScala(ids.theMap.keySet.toList)
+        def builder(keys: List[Int]): List[Int] = {
+            keys match
+                case Nil() => Nil()
+                case x :: xs => ids(x) ++ builder(xs)
+        }
+        builder(keys)
+    }
+    
+}
+
+trait Message
+case class Join(id: Int) extends Message
+case class Empty() extends Message
+case class Msg(text: String) extends Message
+case class StateRT(rt: RoutingTable) extends Message
+case class Error(reason: String) extends Message
+
+
+case class Node(id: Int, replicationFactor: Int) {
+    var network = Network()
+    val routingTable: RoutingTable = RoutingTable(id)
+    var leftLeafSet: List[Int] = List()
+    var rightLeafSet: List[Int] = List()
+    var neighbourhood: List[Int] = List() //incase i need it 
+    // val neighbourHood = ?? 
+
+    // if you already know who it's going to 
+    def send(message: Message, key: Int, to: Int): Unit = {
+        if to == id then handleMessage(message) else network.send(message, key, to)
+    }
+
+    //if you need to figure out who its going to 
+    def route(message: Message, key: Int): Boolean = {
+        val id = routingTable.biggestMatchingPrefix(key)
+        if id == -1 then 
+            false 
+        else 
+            send(message, key, id) 
+            true
+    }
+
+    def receive(message: Message, key: Int): Unit = {
+        if leftLeafSet.head <= key && key <= rightLeafSet.last then
+            def min(nodes: List[Int], nmin: Int, vmin: Int): Int = {
+                nodes match 
+                    case Nil() => nmin
+                    case x :: xs => 
+                        if dist(x,key) < vmin then 
+                            min(xs, x, dist(x,key))
+                        else min(xs, nmin, vmin)
+            }
+            val handler = min(leftLeafSet++rightLeafSet, this.id, dist(key, this.id))
+            if handler == id then handleMessage(message) else send(message, key, handler)
+        else 
+            if !route(message, key) then 
+                def foreach(nodes: List[Int]): Unit = {
+                    nodes match 
+                        case x :: xs =>
+                            if shl(x, key) > shl(id, key) && dist(x, key) < dist(id, key) then
+                                send(message, key, x)
+                            foreach(xs)
+                }
+                foreach(routingTable.idList() ++ leftLeafSet ++ rightLeafSet)
+    }
+
+    //we are definitely handling the message (deliver in Pastry ig)
+    private def handleMessage(msg: Message): Unit = {
+        msg match {
+            case Join(id) => handleJoin(id)
+            case Error(reason) => println(reason)
+        }
+    }
+
+    private def handleJoin(id: Int): Unit = {
+        send(StateRT(this.routingTable), id, id)
+    }
+
+    private def addNeighbor(id: Int): Unit = {
+        if rlt(id, this.id) then 
+
+    }
+    private def removeNeighbor(id: Int): Unit = {
+        
     }
 }
 
-class Network(maxNodes: Int) { 
-    var nodes: Array[Node] = Array.ofDim[Node](maxNodes)
+class Network() { 
+    var nodes: List[Node] = List()
     def add(node: Node) = {
         node.network = this //potential aliasing problem ? 
-        nodes = node +: nodes
+        nodes = node :: nodes
     }
 
-    def send(msg: Message): Purpose = {
-        var x: Purpose = Error(s"Node with id ${msg.to} not found")
-        nodes.foreach(n => if (n.id == msg.to) { x = n.send(msg)})
-        x
+    def send(msg: Message, key: Int, to: Int): Option[Error] = {
+        def foreach(nodes: List[Node]): Option[Error] = {
+            nodes match 
+                case Nil() => Option(Error(s"Node with ID ${to} not found"))
+                case x :: xs => 
+                    if x.id == to then
+                        x.receive(msg, key) 
+                        None()
+                    else 
+                        foreach(xs)
+        }
+        foreach(nodes)
     }
 }
--- pastry/Test.scala
+++ pastry/Test.scala
@@ -1,8 +1,25 @@
 object Test{
+    def shl(a: Int, b: Int): Int = {
+        var i = 0
+        var done = false 
+        while(i <= 32 && !done) {
+            println(s"i $i a ${a>>i} b ${b>>i}")
+            if (a >> i == b >> i) 
+                done = true 
+            else 
+                i += 1
+        }
+        32-i
+    }
+
     def main(args: Array[String]) = {
-        val network: Network = Network(10)
-        val nodes: List[Node] = (for i <- 0 to 5 yield Node(i, 2)).toList
-        nodes.foreach(x => network.add(x))
-        
+        val a = 0b00
+        val b = 0b11
+        val c = 0b1110
+        val d = 0b1101
+        println(a)
+        println(b)
+        println(shl(a,b))
+        println(shl(c,d))
     }
 }
