b89cd70fcd0731c9ef0276a7ddd8f562fdde428a
alex <anithacan01@gmail.com>
2024-12-26 21:56:25 +0100

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- pastry/Test.scala
+++ pastry/Test.scala
@@ -8,22 +8,23 @@ object Test{
     def main(args: Array[String]) = {
         val network = Network( Cell(MutableNil()))
         val rf = 2
-        val node0 = Node(1, rf, network)
-        network.add(node0)
+        // val node0 = Node(1, rf, network)
+        network.add(Node(1, rf, network))
         // val ids = List(2,3)
         // val ids = List(2,1000000000,1000000001,2000000000,2000000001)
         val ids = List(10,11,123456789, 123456799, Int.MaxValue-2,Int.MaxValue-1)
         ids.forall(i => {
             val node = Node(i,rf, network)
             network.add(node)
-            node.forward(Join(node.id, List[Int]()), node.id, node0.id)
+            node.forward(Join(node.id, List[Int]()), node.id, 1)
             true
         })
         // network.nodes.v.forall(x => {
         //         // println(x.mkSting())
         //         true
         //     })
-        node0.send(Msg("hi!", node0.id), 123456800)
+        // node0.send(Msg("hi!", node0.id), 123456800)
+        network.nodes.v.last().send(Msg("hi!", 1), 123456800)
         // val node = Node(0,rf)
         // node.forward(Join(node.id, List[Int]()), node.id, node0.id)
 
--- utils/MutableList.scala
+++ utils/MutableList.scala
@@ -6,6 +6,19 @@ sealed abstract class MutableList[@mutable T] {
     def :+(e: T): MutableList[T] = {
         MutableCons(e, this)
     }
+
+    def length: Int = {
+        this match 
+            case MutableNil() => 0
+            case MutableCons(head, tail) => 1 + tail.length
+    }
+
+    def last(): T = {
+        require(this.length > 0)
+        this match 
+            case MutableCons(head, MutableNil()) => head
+            case MutableCons(head, tail) => tail.last()
+    }
 }
 
 case class MutableCons[@mutable T](head: T, tail: MutableList[T]) extends MutableList[T]
